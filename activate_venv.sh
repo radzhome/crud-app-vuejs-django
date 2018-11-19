2f10dc11aa960ee7adc75e76cebca068b81bea59
radzhome <radzhome@gmail.com>
2018-11-19 13:02:59 -0500

    simplify the project

---
+++ activate_venv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VIRTUALENV_DIR=.venv
+# Source this file to activate.
+. $VIRTUALENV_DIR/bin/activate  # Set up via installer.mac
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,9 +76,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    },
 }
 
 
